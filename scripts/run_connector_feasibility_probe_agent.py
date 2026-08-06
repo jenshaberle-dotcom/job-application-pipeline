4c2bd37129ea9a88f34ba75f53b18c0f243bd655
jenshaberle-dotcom <jens.haberle@gmail.com>
2026-08-06 09:34:43 +0200

    Use generic query-detail S7N runtime

--- scripts/run_connector_feasibility_probe_agent.py
+++ scripts/run_connector_feasibility_probe_agent.py
@@ -8,7 +8,7 @@ import psycopg
 
 from src.config import get_database_config
 from src.search_intelligence.connector_feasibility import OriginCandidate
-from src.search_intelligence.connector_feasibility_runtime import (
+from src.search_intelligence.connector_feasibility_query_runtime import (
     build_connector_feasibility_review,
 )
 
