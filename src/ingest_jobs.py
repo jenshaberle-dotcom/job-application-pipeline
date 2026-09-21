385415ed5d558c8034e1a98faa50bbc9385362ba
jenshaberle-dotcom <jens.haberle@gmail.com>
2026-09-21 10:13:01 +0200

    Align sensor authority contracts and tests

--- src/ingest_jobs.py
+++ src/ingest_jobs.py
@@ -323,7 +323,6 @@ def run_profile(
         repository=repository,
         connector=connector,
         health_repository=health_repository,
-        source_role=profile_source_role(profile),
     )
 
     runner.run(profile_name=profile.profile_name)
